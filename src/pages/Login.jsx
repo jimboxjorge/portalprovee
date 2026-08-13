e182385485f1c79912c45593ad5bd3f8d7ff0ac4
jimboxjorge <jorge_mc19@outlook.es>
2026-08-13 13:06:01 -0600

    nueva conexion de url

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -57,7 +57,7 @@ function Login() {
     try {
 
       const response = await fetch(
-        "https://localhost:7231/api/Login",
+        "https://portal-proveedores-api-fje7dya0h2c7b5bw.mexicocentral-01.azurewebsites.net/api/Login",
         {
           method: "POST",
           headers: {
